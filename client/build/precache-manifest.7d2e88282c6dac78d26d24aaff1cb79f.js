77490572c886f8efb2f51341271057f576ad4453
0tunahanberrin <94645801+0tunahanberrin@users.noreply.github.com>
2023-10-05 13:22:54 +0300

    First Commit

---
+++ client/build/precache-manifest.7d2e88282c6dac78d26d24aaff1cb79f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada1c9e25ec71fee81f65386112c7104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a3d18ca0ee035f35e8",
+    "url": "/static/css/main.4e89baf1.chunk.css"
+  },
+  {
+    "revision": "66089916b007d2d50946",
+    "url": "/static/js/2.a777346e.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.a777346e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a3d18ca0ee035f35e8",
+    "url": "/static/js/main.5d498d0b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
